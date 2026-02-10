40b40e7636d8b33c8934c194f3f6e1909e8917a8
vickykumar-2003 <vickyk15009@gmail.com>
2026-02-10 05:56:22 +0530

    done

---
+++ useeffect/src/App.jsx
@@ -0,0 +1,47 @@
+import{ useEffect,useState } from 'react'
+import './App.css'
+
+function App() {
+
+  // const [count, setCount,] = useState(0);
+  // const [total, setTotal] = useState(0);
+
+  // Variation 1:
+  // useEffect(()=>{
+  //   alert('Hello World');
+  // })
+
+  // veriation 
+  // useEffect(() =>{
+  //   alert('Hello world');
+  // }, []);
+
+  // Variation 3:
+  // useEffect(() =>{
+  //   alert('Hello world');
+  // },[count]);
+
+  // Variation 4:
+  //useEffect(() =>{
+  //   alert('Hello world');
+  // },[count,total]);
+
+
+
+  // function handleClick(){
+  //   setCount(count +1);
+  // }
+
+  // function handleClicktotal(){
+  //   setTotal(total +1);
+  // }
+
+
+  return (
+    <>
+ 
+    </>
+  )
+}
+
+export default App
